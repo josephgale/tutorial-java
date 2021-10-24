e7f2adc447eb9f59e444a9b64fd0e8cbd03e9215
joseph <24618839+josephgale@users.noreply.github.com>
2021-10-23 22:06:27 -0500

    Hacker Rank Day 6 Parial Solution

---
+++ src/hackerrank/Solution6.java
@@ -0,0 +1,9 @@
+package hackerrank;
+
+public class Solution6 {
+    public static void main(String[] args) {
+        String s1 = "This is indexOf method";
+        int index = s1.indexOf(" is ");
+        System.out.println(index);
+    }
+}
